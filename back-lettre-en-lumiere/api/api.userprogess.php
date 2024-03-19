f81f302352142247f062f5423c5196a965d82f14
vtanda01 <vtanda01@etudiant.univ-lr.fr>
2024-03-19 09:50:58 +0100

    fix ajout des entrypoints get  pour userprogress et user

---
+++ back-lettre-en-lumiere/api/api.userprogess.php
@@ -0,0 +1,19 @@
+<?php
+//  ici on recupere les donnée depuis db.sqlite
+ include '../includes/db_connect.php';
+ include '../includes/header.php';
+
+
+    // Endpoint pour récupérer tous les etapes
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        // Exécuter une requête SQL pour récupérer toutes les données des utilisateurs
+        $query = "SELECT * FROM l_USER_PROGRESS WHERE :id='user_progress_id' ";
+        $statement = $pdo->query($query);
+        $stages = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        http_response_code(200);
+        header('Content-Type: application/json');
+        echo json_encode($stages);
+    } 
+
+?>
